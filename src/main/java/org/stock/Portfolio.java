81bc02ea4e1730edd0778fd212badb8b3bfbddcd
arta-crypt <8yt84m.wa3.kg34a@gmail.com>
2024-10-31 22:57:13 +0900

    テスト対象の準備 #1

---
+++ src/main/java/org/stock/Portfolio.java
@@ -0,0 +1,8 @@
+package org.stock;
+
+import java.util.List;
+
+public class Portfolio {
+    private StockService stockService;
+    private List<Stock> stocks;
+}
---
+++ src/main/java/org/stock/StockService.java
@@ -0,0 +1,5 @@
+package org.stock;
+
+public interface StockService {
+    public double getPrice(Stock stock);
+}
