19cebe655b6b2c45ddaa8d65f4bc2258f82ca257
arta-crypt <8yt84m.wa3.kg34a@gmail.com>
2024-11-09 00:40:29 +0900

    テスト作成 #2

--- src/test/java/org/calculator/MathApplicationTest.java
+++ src/test/java/org/calculator/MathApplicationTest.java
@@ -1,5 +1,7 @@
 package org.calculator;
 
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
@@ -7,21 +9,47 @@ import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
-
-@ExtendWith(MockitoExtension.class)
 class MathApplicationTest {
-    @InjectMocks
-    MathApplication mathApplication = new MathApplication();
 
-    @Mock
-    CalculatorService calcService;
 
-    @Test
-    void testAdd() {
-        when(calcService.add(10.0, 20.0)).thenReturn(30.00);
+    @Nested
+    public class MockitoManualTest {
+        private CalculatorService calcService;
+        private MathApplication mathApplication;
 
-        assertEquals(30.0, mathApplication.add(10.0, 20.0), 0);
+        @BeforeEach
+        void initService() {
+            calcService = mock(CalculatorService.class);
+            mathApplication = new MathApplication();
+            mathApplication.setCalcService(calcService);
+        }
+
+        @Test
+        void testAdd() {
+            when(calcService.add(10.0, 20.0)).thenReturn(30.00);
+
+            assertEquals(30.0, mathApplication.add(10.0, 20.0), 0);
+        }
     }
+
+    @Nested
+    @ExtendWith(MockitoExtension.class)
+    class MockitoExtensionInjectMocksTest {
+        @InjectMocks
+        MathApplication mathApplication = new MathApplication();
+
+        @Mock
+        CalculatorService calcService;
+
+        @Test
+        void testAdd() {
+            when(calcService.add(10.0, 20.0)).thenReturn(30.00);
+
+            assertEquals(30.0, mathApplication.add(10.0, 20.0), 0);
+        }
+    }
+
 }
