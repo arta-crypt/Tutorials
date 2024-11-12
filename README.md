3c8420e5605fb93922c26678afbd9f39203d4053
arta-crypt <8yt84m.wa3.kg34a@gmail.com>
2024-11-12 23:42:59 +0900

    データベース接続完了 #5

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # Tutorials
-様々なチュートリアルを試す
+
+## H2データベースサーバ起動コマンド
+
+```bash
+# jarの場所まで移動してから
+cd D:/Program Files (x86)/H2/bin
+java -cp .\h2-2.3.232.jar org.h2.tools.Server -tcp -tcpAllowOthers
+```
--- src/main/java/org/example/JdbcTutorial.java
+++ src/main/java/org/example/JdbcTutorial.java
@@ -1,7 +1,29 @@
 package org.example;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
+
 public class JdbcTutorial {
     public static void main(String[] args) {
-        String url = "";
+        String url = "jdbc:h2:tcp://localhost/D:/Tools/h2/database/samples/db";
+        String user = "sa";
+        String password = "";
+
+        try (Connection connection = DriverManager.getConnection(url, user, password);
+             Statement statement = connection.createStatement()) {
+
+            String query = "SELECT * FROM POKEMON ";
+            ResultSet resultSet = statement.executeQuery(query);
+
+            while (resultSet.next()) {
+                System.out.println("ID: " + resultSet.getInt("ID"));
+                System.out.println("NAME: " + resultSet.getString("NAME"));
+                System.out.println("POKEMON TYPE ID: " + resultSet.getInt("POKEMONTYPE_ID"));
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
